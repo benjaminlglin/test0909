a4acbc4f2601a148a7db4133bbbaa715d17db1c6
ben <youth100@gmail.com>
2021-11-17 23:08:48 +0900

    test

--- content/posts/2021092617/init.md
+++ content/posts/2021092617/init.md
@@ -14,9 +14,16 @@ socialImage: "media/image-2.jpg"
 
 # 学习下面常用句子
 - なんてこった
-- 
+
+<div style="max-width:854px"><div style="position:relative;height:0;padding-bottom:56.25%"><iframe src="https://embed.ted.com/talks/john_doerr_why_the_secret_to_success_is_setting_the_right_goals" width="854" height="480" style="position:absolute;left:0;top:0;width:100%;height:100%" frameborder="0" scrolling="no" allowfullscreen></iframe></div></div>
+
  
 # 第二步：明确与读者的联系（Why）
+<div style="max-width:854px"><div style="position:relative;height:0;padding-bottom:56.25%"><iframe src="https://embed.ted.com/talks/chieh_huang_confessions_of_a_recovering_micromanager" width="854" height="480" style="position:absolute;left:0;top:0;width:100%;height:100%" frameborder="0" scrolling="no" allowfullscreen></iframe></div></div>
+
+
+![](2021-11-06-19-16-57.png)
+
 
 ## 1.勾勒典型场景 用一个大家经常遇到的情景来引入。
 ## 2.塑造典型冲突 场景背后会有一个冲突，可能是未达预期，可能是遭受失败。
---
+++ content/posts/abczero21biz.py
@@ -0,0 +1,54 @@
+import io
+import os
+import sys
+import datetime
+tp1='''---
+title: 在日本買車
+date: "`{0}T23:46:37.121Z"
+template: "post"
+draft: false
+slug: "{1}"
+category: "生活経験"
+tags:
+  - "車"
+  - "日本中国"
+description: "在日本買車的各種準備事項，保險等。"
+socialImage: "media/image-2.jpg"
+---
+
+# 本期重點
+
+ 
+# 第二步：明确与读者的联系（Why）
+
+## 1.勾勒典型场景 用一个大家经常遇到的情景来引入。
+## 2.塑造典型冲突 场景背后会有一个冲突，可能是未达预期，可能是遭受失败。
+## 3.提出核心疑问 为什么会产生这种冲突？我们应该如何解决？
+
+# 第一步：明确主题（What）
+
+# 第三步：找到关键答案（How）
+
+## 1.推理法。层层推进，找到问题的根源。
+
+## 2.归纳法。把一系列答案，归总于几个大类，然后分类表述。
+
+## 3.步骤法。给出具体的操作步骤。
+'''
+os.chdir('C:\\Users\\youth\\Documents\\gitdocs\\test0909\\content\\posts')
+dt_now = datetime.datetime.now()
+fn=dt_now.strftime('%Y%m%d%H')
+fn2=dt_now.strftime('%Y/%m/%d')
+fn3=dt_now.strftime('%Y-%m-%d')
+os.mkdir(fn)
+print(fn)
+print(dt_now)
+args=sys.argv
+slug="a-article"
+if (len(args)==2):
+    slug=args[1]
+    print(slug)
+
+with open("./{}/init.md".format(fn), mode='w',encoding='utf8') as f:
+    f.write(tp1.format(fn3,slug))    
+    
